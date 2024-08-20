afa2ec286730de83a2799f27a76389b8110ed6f5
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-08-19 23:44:19 -0300

    Training Python - Section basics exercise

---
+++ Python3_by_LuizOtavio/Section_Basics/c1.py
@@ -0,0 +1,16 @@
+# General one line comment
+print(1098) #Another test ^^
+
+"""
+It's a DocString
+Not a commentary
+
+"""
+
+'''
+It's a DocString too,
+but you can use as a note
+
+'''
+
+print("finished")
