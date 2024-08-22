1d07fe7cf5a6308a08e027f998f2ef628c8181be
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-08-21 22:54:50 -0300

    Training Python - About comments

---
+++ Python3_by_LuizOtavio/Section_Basics/c3.py
@@ -0,0 +1,24 @@
+# Python: tipagem dinâmica/forte
+
+#str -> string -> texto
+
+#Aspas duplas
+print("texto")
+print("I'm")
+
+#Aspas simples
+print('Uma " aspa')
+print('Outro texto')
+
+
+#Escape
+
+print("API\'s")
+
+#r - mostra o caracter \ de escape
+print(r"API\'s e \"notes\"")
+
+#Aspas Duplas e aspas simples
+
+'''Doctring'''
+
