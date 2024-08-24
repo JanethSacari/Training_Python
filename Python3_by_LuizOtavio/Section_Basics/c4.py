396496d4bbd8d7609b700d0596bf88bf635d9f2f
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-08-23 23:27:20 -0300

    Training Python - About primitive numbers

---
+++ Python3_by_LuizOtavio/Section_Basics/c4.py
@@ -0,0 +1,17 @@
+# Tipos int e float
+# type('') mostra o tipo que o python inferiu à função
+
+#int
+print (8)
+print(0)
+print(-8)
+
+
+#floatm ,
+print(2.2)
+print(1.1, 6.7)
+print(-3.1)
+
+#Verificando o tipo
+print(type(5))
+print(type(1.1),    type(30)   ,type('Letra'))
