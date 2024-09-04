5f99fb33d5ad0948caa3fb3d09ebad196eb417a1
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-09-03 23:25:38 -0300

    Training Python - About boolean type

---
+++ Python3_by_LuizOtavio/Section_Basics/c5.py
@@ -0,0 +1,8 @@
+#Dados do tipo bool (boolean)
+#Pode ser apenas 'True' ou 'False'
+#Usados em conjunto com expressões como == ou outra expressão de comparação
+
+print(10 == 10) # True
+print(2 == 3) #False
+
+print (type(5 == 6))
