d43fd72c4f9d18cbb010b208f3224ff2aee26491
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-09-04 22:15:58 -0300

    Training Python - Polimorfismo and casting

---
+++ Python3_by_LuizOtavio/Section_Basics/c6.py
@@ -0,0 +1,14 @@
+#Conversão de tipos
+# type convertion, type casting, coercion
+
+#str, int, float, bool
+
+#polimorfismo
+print(1 + 1)
+print('a' + 'b')
+
+#convertendo
+print(int('2') + 2)
+print(float('2.2') + 2)
+print(bool('Anything')) #True
+print(str(22) + ' maças')
