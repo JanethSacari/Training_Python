d0d22d653fc994a5e4a5525cd19220ba69dd1440
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-09-11 22:19:26 -0300

    Training Python - Math operations.

---
+++ Python3_by_LuizOtavio/Section_Basics/c7.py
@@ -0,0 +1,9 @@
+#PEP8:variaveis iniciadas com letras minusculas, separado por underline _
+# Sinal = é atribuição
+
+nome_completo = 'Ayame Hinode'
+ocupacao = 'estudante'
+afirmacao_f = bool(0)
+afirmacao_v = bool(True)
+
+print(nome_completo, ocupacao, ' tem cinco gatos', afirmacao_f, 'or', afirmacao_v)
