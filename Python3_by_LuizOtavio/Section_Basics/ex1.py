17da98ed0d2e563fcc009af5efc230f03abcfedd
Janeth Mamani Sacari <janeth.mamani.sacari@gmail.com>
2024-10-05 20:29:21 -0300

    Training Python - Exercise 1

---
+++ Python3_by_LuizOtavio/Section_Basics/ex1.py
@@ -0,0 +1,14 @@
+nome = 'Janeth'
+sobrenome = 'Mamani'
+idade = 0
+ano_nascimento = 0
+maior_de_idade = idade>=18
+altura_metros = 2
+
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
